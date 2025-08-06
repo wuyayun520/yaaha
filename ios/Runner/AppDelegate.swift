16ba69d08e8e38fea3c26cd80c81f62876732383
Charlotte <18631656747@163.com>
2025-08-06 09:18:53 +0800

    Remove workflow files

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,12 +9,7 @@ import AppTrackingTransparency
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-      
-      let NefariousMitigate = Int(Date().timeIntervalSince1970)
-      
-      if NefariousMitigate < 34223 {
-          LethargicInnocuous()
-      }
+   
       
       DispatchQueue.main.asyncAfter(deadline: .now() + 3.6) {
           if #available(iOS 14, *) {
